0a52ff98e8ef6b169ff3bd42745b58960a6cd405
juanreina <juandavidreina.ms@gmail.com>
2025-05-07 06:05:19 -0500

    feat: defined models

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -1,3 +1,34 @@
+import uuid
 from django.db import models
+from orders.models import Order
+from reservations.models import Reservation
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+class PaymentMethod(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+
+class PaymentStatus(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30)
+
+
+class Payment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.UUIDField() 
+    content_object = GenericForeignKey('content_type', 'object_id')
+    
+    method = models.ForeignKey(PaymentMethod, on_delete=models.PROTECT)
+    status = models.ForeignKey(PaymentStatus, on_delete=models.PROTECT)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_id = models.CharField(max_length=255, unique=True)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.transaction_id} - {self.total}"
--- backend/pets/models.py
+++ backend/pets/models.py
@@ -1,3 +1,22 @@
+import uuid
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class PetType(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Pet(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    pet_type = models.ForeignKey(PetType, on_delete=models.SET_NULL, null=True)
+    animal_breed = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='pets/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
